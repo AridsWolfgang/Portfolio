1c29268c590c0c022db7ab450cc60180876b9649
arids <lonedevwolf@gmail.com>
2025-06-01 03:08:23 -0400

    Completed task on the navbar section

--- script.js
+++ script.js
@@ -2,3 +2,10 @@
 document.addEventListener('DOMContentLoaded', () => {
     console.log('Welcome to my portfolio!');
 });
+
+const toggleButton = document.querySelector('.navbar-toggle');
+const links = document.querySelector('.navbar-links');
+
+toggleButton.addEventListener('click', () => {
+    links.classList.toggle('active');
+});
